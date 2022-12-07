97ccbf485f561d52aa007567296294a49dd04ed7
buicongdanh <64085482+buicongdanh@users.noreply.github.com>
2022-12-07 16:40:12 +0700

    ?

--- Script/stage.sql
+++ Script/stage.sql
@@ -20,24 +20,25 @@ DROP TABLE vaccines_by_age_phu
 -- Columns: Outcome, Age, Gender, Reporting_PHU, SpecimenDate, CaseReported_Date, PHUCity, TestReported_Date, CaseAcquisition_info, AccurateEpisode_Dt, PHU_Address, PHU_Website, OutbreakRelated, PHU_Latitude, PHU_Longitude, PHU_Postal_Code
 
 CREATE TABLE Cases_Report (
-    Outcome nvarchar(MAX),
-    Age nvarchar(MAX),
-    Gender nvarchar(MAX),
-    Reporting_PHU nvarchar(MAX),
+    Outcome nvarchar(300),
+    Age nvarchar(300),
+    Gender nvarchar(300),
+    Reporting_PHU nvarchar(300),
     SpecimenDate date,
     CaseReported_Date date,
-    PHUCity nvarchar(MAX),
+    PHUCity nvarchar(300),
     TestReported_Date date,
-    CaseAcquisition_info nvarchar(MAX),
+    CaseAcquisition_info nvarchar(300),
     AccurateEpisode_Dt date,
-    PHU_Address nvarchar(MAX),
-    PHU_Website nvarchar(MAX),
-    OutbreakRelated nvarchar(MAX),
+    PHU_Address nvarchar(300),
+    PHU_Website nvarchar(300),
+    OutbreakRelated nvarchar(300),
     PHU_Latitude float,
     PHU_Longitude float,
-    PHU_Postal_Code nvarchar(MAX),
+    PHU_Postal_Code nvarchar(300),
 	Create_day date,
 	Update_day date,
+	_Status int
 )
 GO
 -- Table: Compiled_COVID-19_Case_Details_Canada_csv
@@ -47,14 +48,15 @@ CREATE TABLE Compiled_COVID_19_Case_Details_Canada (
     ObjectId bigint,
     row_id bigint,
     date_reported datetimeoffset,
-    health_region nvarchar(MAX),
-    age_group nvarchar(MAX),
-    gender nvarchar(MAX),
-    exposure nvarchar(MAX),
-    case_status nvarchar(MAX),
-    province nvarchar(MAX),
+    health_region nvarchar(300),
+    age_group nvarchar(300),
+    gender nvarchar(300),
+    exposure nvarchar(300),
+    case_status nvarchar(300),
+    province nvarchar(300),
 	Create_day date,
 	Update_day date,
+	_Status int
 )
 GO
 -- Table: ongoing_outbreaks_phu_csv
@@ -63,10 +65,11 @@ GO
 CREATE TABLE ongoing_outbreaks_phu (
     date date,
     phu_num bigint,
-    outbreak_group nvarchar(MAX),
+    outbreak_group nvarchar(300),
     number_ongoing_outbreaks bigint,
 	Create_day date,
 	Update_day date,
+	_Status int
 )
 GO
 -- Table: Public_health_unit_xlsx
@@ -74,26 +77,28 @@ GO
 
 CREATE TABLE Public_health_unit (
     PHU_ID bigint,
-    Reporting_PHU nvarchar(MAX),
-    Reporting_PHU_Address nvarchar(MAX),
-    Reporting_PHU_City nvarchar(MAX),
-    Reporting_PHU_Postal_Code nvarchar(MAX),
-    Reporting_PHU_Website nvarchar(MAX),
+    Reporting_PHU nvarchar(300),
+    Reporting_PHU_Address nvarchar(300),
+    Reporting_PHU_City nvarchar(300),
+    Reporting_PHU_Postal_Code nvarchar(300),
+    Reporting_PHU_Website nvarchar(300),
     Reporting_PHU_Latitude float,
     Reporting_PHU_Longitude float,
 	Create_day date,
 	Update_day date,
+	_Status int
 )
 GO
 -- Table: Public_Health_Units_GROUP_xlsx
 -- Columns: PHU_Group, PHU_City, PHU_region
 
 CREATE TABLE Public_Health_Units_GROUP (
-    PHU_Group nvarchar(MAX),
-    PHU_City nvarchar(MAX),
-    PHU_region nvarchar(MAX),
+    PHU_Group nvarchar(300),
+    PHU_City nvarchar(300),
+    PHU_region nvarchar(300),
 	Create_day date,
 	Update_day date,
+	_Status int
 )
 GO
 -- Table: vaccines_by_age_phu_csv
@@ -102,13 +107,14 @@ GO
 CREATE TABLE vaccines_by_age_phu (
     Date date,
     PHU_ID bigint,
-    Agegroup nvarchar(MAX),
+    Agegroup nvarchar(300),
     At_least_one_dose_cumulative bigint,
     Second_dose_cumulative bigint,
     fully_vaccinated_cumulative bigint,
     third_dose_cumulative bigint,
 	Create_day date,
 	Update_day date,
+	_Status int
 )
 GO
 -- Table: vaccines_by_age_phu_xlsx
@@ -120,4 +126,8 @@ SELECT * FROM ongoing_outbreaks_phu
 SELECT * FROM Public_health_unit
 SELECT * FROM Public_Health_Units_GROUP
 SELECT * FROM vaccines_by_age_phu
+
+select distinct phu.phu_id, phu.reporting_phu, phu.reporting_phu_city, phg.phu_city
+from Public_health_unit phu left join Public_Health_Units_GROUP phg on (phu.reporting_phu_city = phg.phu_city)
+
 
