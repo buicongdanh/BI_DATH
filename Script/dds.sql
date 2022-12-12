1d01b64470b981c2b0c06ece4b512f4cfcbd353c
buicongdanh <64085482+buicongdanh@users.noreply.github.com>
2022-12-13 03:53:01 +0700

    etl complete

--- Script/dds.sql
+++ Script/dds.sql
@@ -1,313 +1,135 @@
 create database DATH_DDS
 go
 use DATH_DDS
-
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[sp_insertNgayThangQuyNam]') AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
-  DROP PROCEDURE [dbo].[sp_insertNgayThangNam]
-GO
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[NgayTrongThang]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[NgayTrongThang]
-GO
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[QuyTrongNam]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[QuyTrongNam]
-GO
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[Date]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[Date]
-GO
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[PHU]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[PHU]
-GO
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[PHU_GROUP]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[PHU_GROUP]
-GO
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[PHU_City]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[PHU_City]
-GO
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[Geography]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[Geography]
 GO
 
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[Gender]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[Gender]
-GO
-
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[Age]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[Age]
-GO
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[Case_Status]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[Case_Status]
-GO
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[Exposure]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[Exposure]
-GO
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[FactCase]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-  DROP TABLE [dbo].[FactCase]
-GO
-/*
-CREATE TABLE [dbo].[ThangTrongNam] (
-  [IDThang] int IDENTITY(1, 1) NOT NULL,
-  [Thang] int NULL,
-  [Nam] int NULL
-)
-ON [PRIMARY]
-GO
-
---
--- Definition for table NgayTrongThang : 
---
-
-CREATE TABLE [dbo].[NgayTrongThang] (
-  [IDNgay] int IDENTITY(1, 1) NOT NULL,
-  [IDThang] int NULL,
-  [Ngay] int NULL
-)
-ON [PRIMARY]
-GO
-
-CREATE TABLE [dbo].[ThangTrongQuy] (
-  [IDThang] int IDENTITY(1, 1) NOT NULL,
-  [IDQuy] int NULL,
-  [Thang] int NULL
-)
-ON [PRIMARY]
-GO
-
---
--- Definition for table NgayTrongThang : 
---
-
-CREATE TABLE [dbo].[QuyTrongNam] (
-  [IDQuy] int IDENTITY(1, 1) NOT NULL,
-  [Quy] int NULL,
-  [Nam] int NULL
-)
-ON [PRIMARY]
-GO
-*/
-
-drop table [dim_Date]
 CREATE TABLE [dbo].[dim_Date] (
-  [IDDate] int IDENTITY(1, 1) NOT NULL,
-  FullDateTime datetime NULL,
-  Ngay int NULL,
-  Thang int NULL,
-  Quy int NULL,
-  Nam int NULL
+	[IDDate]		bigint IDENTITY(1, 1) NOT NULL,
+	FullDateTime	date NULL,
+	Ngay			int NULL,
+	Thang			int NULL,
+	Quy				int NULL,
+	Nam				int NULL
 )
-ON [PRIMARY]
-GO
 
-drop table dim_PHU
+
 CREATE TABLE dim_PHU (
-    PHU_ID bigint IDENTITY(1,1) PRIMARY KEY,
-	PHU_ID_NK bigint,
-    Reporting_PHU varchar(MAX) NULL,
-    Reporting_PHU_Address varchar(MAX) NULL,--Changing
-    PHU_City_ID bigint,--Historical
-    Reporting_PHU_Postal_Code varchar(MAX) NULL, --Changing
-    Reporting_PHU_Website varchar(MAX) NULL, --Changing
+    PHU_ID				bigint IDENTITY(1,1) PRIMARY KEY,
+	PHU_ID_NK			bigint,
+	PHU_City_ID			bigint,--Historical
+	PHU_City			nvarchar(300),
+	PHU_Group_ID		bigint,
+	PHU_GROUP			nvarchar(300),
+    Reporting_PHU		nvarchar(300) NULL,
+    Reporting_PHU_Address nvarchar(300) NULL,--Changing
+    Reporting_PHU_Postal_Code nvarchar(300) NULL, --Changing
+    Reporting_PHU_Website nvarchar(300) NULL, --Changing
     Reporting_PHU_Latitude float NULL,--Changing
     Reporting_PHU_Longitude float NULL,--Changing
-	_Status int
+	Create_day		date,
+	Update_day		date,
+	_Status				int
     -- PHU_City_ID bigint FOREIGN KEY REFERENCES PHU_City(PHU_City_ID)
 )
 
-CREATE TABLE PHU_GROUP (
-    PHU_GROUP_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    PHU_GROUP varchar(MAX) NULL,--Changing
-    Create_day date,
-	Update_day date,
-	_Status int
-)
-
-CREATE TABLE PHU_City (
-    PHU_City_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    PHU_City varchar(MAX) NULL,--Changing
-    PHU_Group_ID bigint, --Historical
-	_Status int
-)
-
-CREATE TABLE [dbo].[dim_Geography] (
-  [IDGeography] bigint IDENTITY(1, 1) NOT NULL,
-  [PHU_ID] bigint NULL, --Historical
-  [Reporting_PHU] varchar(300) NULL,
-  [PHU_City_ID] bigint NULL, --Historical
-  _Status int
-)
-ON [PRIMARY]
-GO
-
 CREATE TABLE dim_Gender (
-    Gender_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Gender nvarchar(300) NULL,
-	SourceID int,
-	_Status int,
+    Gender_ID		bigint IDENTITY(1,1) PRIMARY KEY,
+	Gender_NK		bigint,
+    Gender_Name		nvarchar(300) NULL,
+	SourceID		bigint,
+	Create_day		date,
+	Update_day		date,
+	_Status			int,
 )
 
-CREATE TABLE Age (
-    Age_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Age varchar(300) NULL,
-	SourceID int,
-	_Status int
+CREATE TABLE dim_Age (
+    Age_ID			bigint IDENTITY(1,1) PRIMARY KEY,
+	Age_NK			bigint,
+    Age_Name		nvarchar(300) NULL,
+	SourceID		bigint,
+	Create_day		date,
+	Update_day		date,
+	_Status			int
 )
 
 CREATE TABLE dim_Case_Status (
-    Case_Status_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Case_Status varchar(MAX) NULL,
-	_Status int
+    Case_Status_ID	bigint IDENTITY(1,1) PRIMARY KEY,
+	Case_Status_NK	bigint,
+    Case_Status_Name	nvarchar(300) NULL,
+	Source_ID		bigint,
+	Create_day		date,
+	Update_day		date,
+	_Status			int
 )
 
-CREATE TABLE dim_Exposure (
-    Exposure_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Exposure varchar(300) NULL,
-	_Status int
+CREATE TABLE dim_Outcome (
+    Outcome_ID	bigint IDENTITY(1,1) PRIMARY KEY,
+	Outcome_NK	bigint,
+    Outcome_Name	nvarchar(300) NULL,
+	Source_ID		bigint,
+	Create_day		date,
+	Update_day		date,
+	_Status			int
 )
 
-CREATE TABLE  [dbo].[Fact_Case] (
-  [IDDate] int NOT NULL,
-  [PHU_ID] bigint NOT NULL,
-  [Age_ID] bigint NOT NULL,
-  [Gender_ID] bigint NOT NULL,
-  [Case_Status_ID] bigint NOT NULL,
-  [ToTalCase] bigint NULL,
-  primary key (IDDate,PHU_ID,Age_ID,Gender_ID,Case_Status_ID)
+CREATE TABLE  [dbo].[Fact_Case] (	
+  --Dim
+	[IDDate]		bigint NOT NULL,
+	[PHU_ID]		bigint NOT NULL,
+	[Age_ID]		bigint NOT NULL,
+	[Gender_ID]		bigint NOT NULL,
+	[Outcome_ID]	bigint NOT NULL,
+	[Case_Status_ID] bigint NOT NULL,
+	[SourceID]		bigint NOT NULL,
+  --Measure
+	[ToTalCase] bigint NULL,
+	primary key (IDDate, PHU_ID, Age_ID, Gender_ID, Case_Status_ID, Outcome_ID)
 )
 
 CREATE TABLE [dbo].[Fact_Vacinated](
-  [IDDate] int NOT NULL,
-  [PHU_ID] bigint NOT NULL,
-  [Age_ID] bigint NOT NULL,
-  [TotalVacinated] int NULL,
-  primary key (IDDate,PHU_ID,Age_ID)
+  --Dim
+	[IDDate]		bigint NOT NULL,
+	[PHU_ID]		bigint NOT NULL,
+	[Age_ID]		bigint NOT NULL,
+  --Measure
+	OneDoseCumulative	bigint null,
+	SecondDoseCumulative bigint null,
+	FullyVaccinated		bigint null,
+	ThirdDoseCumulative	bigint null,
+	primary key (IDDate, PHU_ID, Age_ID)
 )
 
-
+Create table SourceID (
+	Source_ID	bigint PRIMARY KEY,
+	SourceName	nvarchar(300)
+)
+/*
+1 - Case_Report
+2 - Compiled_COVID_19_Case_Details_Canada
+3 - vaccines_by_age_phu
+*/
+Create table _Status (
+	Status_ID		int PRIMARY KEY,
+	Status_Name		nvarchar(300)
+)
 
 --Them khoa ngoai neu can
 ALTER TABLE PHU_City ADD FOREIGN KEY (PHU_Group_ID) REFERENCES PHU_Group(PHU_Group_ID)
 
 -- PHU_NDS(PHU_City_ID) REFERENCES PHU_City_NDS(PHU_City_ID)
 ALTER TABLE PHU ADD FOREIGN KEY (PHU_City_ID) REFERENCES PHU_City(PHU_City_ID)
-
-
-create proc [dbo].sp_insertNgayThangQuyNam
-as
-begin
-	declare @lset datetime
-	select @lset =NgayCapNhat from METADATA.DBO.DDS where TenBang='Ngay'
-	declare @counter datetime
-	set @counter = @lset
-	declare @thang int
-	set @thang = Month(@counter)
-	declare @idthang int
-	declare @quy int
-	declare @idquy int 
-	if (@thang = 1|2|3)
-	begin
-		set @quy=1
-	end
-	if (@thang = 4|5|6)
-	begin
-		set @quy=2
-	end
-	if (@thang = 7|8|9)
-	begin
-		set @quy=3
-	end
-	if (@thang = 10|11|12)
-	begin
-		set @quy=4
-	end
-	if (@thang = Month(getdate()) or year(@counter)=year(getdate()))
-	begin
-		while @counter < getdate()
-		begin
-				select @idthang=IDThang from ThangTrongQuy where Thang=@thang and IDQuy=(Select IDQuy from QuyTrongNam where Nam=year(@counter))
-				insert into NgayTrongThang(IDThang,Ngay) values(@idthang,day(@counter))
-				set @counter = DATEADD(day,1,@counter)
-		end
-	end
-	else
-	begin
-		while @counter < getdate()
-		begin
-			set @thang = Month(@counter)
-
-			insert into QuyTrongNam(Quy,Nam) values(@quy,year(@counter))
-			select @idquy=IDQuy from QuyTrongNam where Quy=@quy
-			insert into ThangTrongQuy(Thang,IDQuy) values(@thang, @idquy)
-			while (Month(@counter) = @thang and @counter < getdate()) --insert ngày cho tháng
-			begin
-				select @idthang=IDThang from ThangTrongQuy where Thang=@thang and IDQuy=(Select IDQuy from QuyTrongNam where Nam=year(@counter))
-				insert into NgayTrongThang(IDThang,Ngay) values(@idthang,day(@counter))
-				set @counter = DATEADD(day,1,@counter)
-			end
-			
-		end
-	end
-end
-GO
-
-
-create proc [dbo].sp_insertdata
-as
-begin
-	
-	declare @ddate datetime
-	declare @ngay int
-	set @ngay=1
-	declare @thang int
-	set @thang =1
-	declare @nam int
-	set @nam= 2020
-	declare @quy int =0
-	while @nam<2023
-	begin
-		while @thang<13
-		begin
-			while @ngay<32
-			begin 	
-				if (@thang = 1|2|3)
-				begin
-					set @quy=1
-				end
-				if (@thang = 4|5|6)
-				begin
-					set @quy=2
-				end
-				if (@thang = 7|8|9)
-				begin
-					set @quy=3
-				end
-				if (@thang = 10|11|12)
-				begin
-					set @quy=4
-				end
-				SELECT @ddate= CONVERT(DATETIME,CONCAT(@ngay,'/',@thang,'/',@nam),103)
-				insert into dim_Date(FullDateTime,Ngay,Thang,Quy,Nam) values(@ddate,@ngay,@thang,@quy,@nam)
-				set @ngay=@ngay+1
-			end
-			set @ngay=1
-			set @thang=@thang + 1
-		end
-		set @thang=1
-		set @nam=@nam + 1
-	end 
-end
-
-EXEC sp_insertdata
-select * from dim_Date
-
+truncate table dim_Date
 --FILL dim_date data
 use DATH_DDS
 go
 create proc add_date
---alter proc fill_date
+
+alter proc add_date
 As
 begin
 	declare @req_start_date Datetime,
 			@req_end_date Datetime
-	SET @req_start_date='2020-01-01'
+	SET @req_start_date='2018-01-01'
 	SET @req_end_date='2023-12-31'
 	;WITH X AS 
     (
@@ -323,4 +145,6 @@ begin
 END
 
 use DATH_DDS
-exec fill_date
+exec add_date
+
+select * from dim_date
--- Script/nds.sql
+++ Script/nds.sql
@@ -9,7 +9,6 @@ GO
 DROP DATABASE DATH_NDS
 GO
 -- DROP TABLE --------------------------------------------------------------------------------
-
 DROP TABLE COMPILED_COVID_19_CASE_DETAILS_CANADA_NDS
 DROP TABLE CASES_REPORT_NDS
 DROP TABLE VACCINES_BY_AGE_PHU_NDS
@@ -41,28 +40,29 @@ CREATE TABLE PHU_GROUP_NDS (
 -- Table: PHU_City_NDS
 -- Column: PHU_City_ID, PHU_City, PHU_Group_ID
 CREATE TABLE PHU_City_NDS (
-    PHU_City_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    PHU_City nvarchar(300) NULL,
-	Create_day date,
-	Update_day date,
+    PHU_City_ID		bigint IDENTITY(1,1) PRIMARY KEY,
+    PHU_City		nvarchar(300) NULL,
+	PHU_Region		nvarchar(300) NULL,	
+	Create_day		date,
+	Update_day		date,
     -- PHU_Group_ID bigint FOREIGN KEY REFERENCES PHU_GROUP(PHU_GROUP_ID)
 )
 
 -- Table: PHU_NDS
 -- Column: PHU_ID, Reporting_PHU, Reporting_PHU_Address, PHU_City_ID, Reporting_PHU_Postal_Code, Reporting_PHU_Website, Reporting_PHU_Latitude, Reporting_PHU_Longitude
 CREATE TABLE PHU_NDS (
-    PHU_ID bigint IDENTITY(1,1) PRIMARY KEY,
-	PHU_ID_NK bigint NULL,
-    Reporting_PHU nvarchar(300) NULL,
+    PHU_ID			bigint IDENTITY(1,1) PRIMARY KEY,
+	PHU_ID_NK		bigint NULL,
+    Reporting_PHU	nvarchar(300) NULL,
     Reporting_PHU_Address nvarchar(300) NULL,
-    PHU_City_ID bigint,
+    PHU_Group_ID		bigint,
     Reporting_PHU_Postal_Code nvarchar(300) NULL,
     Reporting_PHU_Website nvarchar(300) NULL,
     Reporting_PHU_Latitude float NULL,
     Reporting_PHU_Longitude float NULL,
-	Create_day date,
-	Update_day date,
-	_Status int
+	Create_day		date,
+	Update_day		date,
+	_Status			int
     -- PHU_City_ID bigint FOREIGN KEY REFERENCES PHU_City(PHU_City_ID)
 )
 
@@ -70,16 +70,16 @@ CREATE TABLE PHU_NDS (
 -- Column: Vaccines_by_age_PHU_ID, Date, PHU_ID, Age_Group_ID, At_least_one_dose_cumulative, Second_dose_cumulative, fully_vaccinated_cumulative, third_dose_cumulative
 CREATE TABLE Vaccines_by_age_PHU_NDS (
     Vaccines_by_age_PHU_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Date date NULL,
-    PHU_ID bigint,
-    Age_Group_ID bigint,
+    Date			date NULL,
+    PHU_ID			bigint,
+    Age_Group_ID	bigint,
     At_least_one_dose_cumulative bigint,
     Second_dose_cumulative bigint,
     fully_vaccinated_cumulative bigint,
     third_dose_cumulative bigint,
-	Create_day date,
-	Update_day date,
-	_Status int
+	Create_day		date,
+	Update_day		date,
+	_Status			int
     -- PHU_ID bigint FOREIGN KEY REFERENCES PHU(PHU_ID),
     -- Age_Group_ID bigint FOREIGN KEY REFERENCES Age_Group(Age_Group_ID)
 )
@@ -89,21 +89,21 @@ CREATE TABLE Vaccines_by_age_PHU_NDS (
 CREATE TABLE Outbreaks_Group_NDS (
     Outbreaks_Group_ID bigint IDENTITY(1,1) PRIMARY KEY,
     Outbreaks_Group nvarchar(300) NULL,
-	Create_day date,
-	Update_day date,
+	Create_day		date,
+	Update_day		date,
 )
 
 -- Table: Ongoing_Outbreaks_PHU_NDS
 -- Column: Ongoing_Outbreaks_PHU_ID, Date, PHU_ID, Outbreaks_Group_ID, Number_Ongoing_Outbreaks
 CREATE TABLE Ongoing_Outbreaks_PHU_NDS (
     Ongoing_Outbreaks_PHU_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Date date NULL,
-    PHU_ID bigint,
+    Date			date NULL,
+    PHU_ID			bigint,
     Outbreaks_Group_ID bigint,
     Number_Ongoing_Outbreaks bigint,
-	Create_day date,
-	Update_day date,
-	_Status int
+	Create_day		date,
+	Update_day		date,
+	_Status			int
     -- PHU_ID bigint FOREIGN KEY REFERENCES PHU(PHU_ID),
     -- Outbreaks_Group_ID bigint FOREIGN KEY REFERENCES Outbreaks_Group(Outbreaks_Group_ID)
 )
@@ -113,101 +113,117 @@ CREATE TABLE Ongoing_Outbreaks_PHU_NDS (
 CREATE TABLE Case_Acquisition_Info_NDS (
     Case_Acquisition_Info_ID bigint IDENTITY(1,1) PRIMARY KEY,
     Case_Acquisition_Info nvarchar(300) NULL,
-	Create_day date,
-	Update_day date
+	SourceID		bigint,
+	Create_day		date,
+	Update_day		date
 )
-
 
 -- Table: Cases_Report_NDS
 -- Column: Case_Report_ID, Outcome, Age_Group_ID, Gender_ID, PHU_ID, Specimen_Date, Case_Reported_Date, Test_Reported_Date, Case_Acquisition_Info_ID, Accurate_Episode_Date, Outbreak_Related
 CREATE TABLE Cases_Report_NDS (
     Case_Report_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Outcome nvarchar(300),
-    Age_Group_ID bigint,
-    Gender_ID bigint,
-    PHU_ID bigint,  
-    Specimen_Date date,
+    Outcome_ID			bigint,
+    Age_Group_ID	bigint,
+    Gender_ID		bigint,
+    PHU_ID			bigint,  
+    Specimen_Date	date,
     Case_Reported_Date date,
     Test_Reported_Date date,
     Case_Acquisition_Info_ID bigint,
     Accurate_Episode_Date date,
     Outbreak_Related nvarchar(300),
-	Create_day date,
-	Update_day date,
-	_Status int
+	Create_day		date,
+	Update_day		date,
+	_Status			int
     -- Age_Group_ID bigint FOREIGN KEY REFERENCES Age_Group(Age_Group_ID),
     -- Case_AcquisitionInfo_ID bigint FOREIGN KEY REFERENCES Case_Acquisition_Info(Case_Acquisition_Info_ID)
 )
+CREATE TABLE Outcome_NDS (
+    Outcome_ID		bigint IDENTITY(1,1) PRIMARY KEY,
+    Outcome			nvarchar(300) NULL,
+	SourceID		bigint,
+	Create_day		date,
+	Update_day		date
+)
+
 
 -- Table: Exposure_NDS
 -- Column: Exposure_ID, Exposure
 CREATE TABLE Exposure_NDS (
-    Exposure_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Exposure nvarchar(300) NULL,
-	Create_day date,
-	Update_day date
+    Exposure_ID		bigint IDENTITY(1,1) PRIMARY KEY,
+    Exposure		nvarchar(300) NULL,
+	SourceID		bigint,
+	Create_day		date,
+	Update_day		date
 )
 
 -- Table: Case_Status_NDS
 -- Column: Case_Status_ID, Case_Status
 CREATE TABLE Case_Status_NDS (
-    Case_Status_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Case_Status nvarchar(300) NULL,
-	Create_day date,
-	Update_day date
+    Case_Status_ID	bigint IDENTITY(1,1) PRIMARY KEY,
+    Case_Status		nvarchar(300) NULL,
+	SourceID		bigint,
+	Create_day		date,
+	Update_day		date
 )
 
 -- Table: Age_Group_NDS
 -- Column: Age_Group_ID, Age_Group, Source
 CREATE TABLE Age_Group_NDS (
-    Age_Group_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Age_Group nvarchar(300) NULL,
-	SourceID bigint,
-	Create_day date,
-	Update_day date
+    Age_Group_ID	bigint IDENTITY(1,1) PRIMARY KEY,
+    Age_Group		nvarchar(300) NULL,
+	SourceID		bigint,
+	Create_day		date,
+	Update_day		date
 )
 
 -- Table: Gender_NDS
 -- Column: Gender_ID, Gender, Source
 CREATE TABLE Gender_NDS (
-    Gender_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Gender nvarchar(300) NULL,
-	SourceID bigint,
-	Create_day date,
-	Update_day date
+    Gender_ID		bigint IDENTITY(1,1) PRIMARY KEY,
+    Gender			nvarchar(300) NULL,
+	SourceID		bigint,
+	Create_day		date,
+	Update_day		date,
 )
+
+CREATE TABLE Province_NDS (
+	Province_ID		bigint IDENTITY(1,1) PRIMARY KEY,
+	Province_Name	nvarchar(300),
+	Create_day		date,
+	Update_day		date,
+	_Status			int
+)
+
 -- Table: Compiled_COVID_19_Case_Details_Canada_NDS
--- Column: Compiled_ID, Date_Reported, PHU_ID, Age_Group_ID, Gender_ID, Test_Reported_Date, Exposure_ID, Case_Status_ID, Province
 CREATE TABLE Compiled_COVID_19_Case_Details_Canada_NDS (
-    Compiled_ID bigint IDENTITY(1,1) PRIMARY KEY,
-    Date_Reported datetimeoffset,
-    Health_Region nvarchar(300),
-    Age_Group_ID bigint,
-    Gender_ID bigint,
-    --Test_Reported_Date date,
-    Exposure_ID bigint,
-    Case_Status_ID bigint,
-    Province nvarchar(300),
-	Create_day date,
-	Update_day date,
-	_Status int
+    Compiled_ID		bigint IDENTITY(1,1) PRIMARY KEY,
+    Date_Reported	date,
+    PHU_ID			bigint,
+    Age_Group_ID	bigint,
+    Gender_ID		bigint,
+    Exposure_ID		bigint,
+    Case_Status_ID	bigint,
+    Province_ID		bigint,
+	Create_day		date,
+	Update_day		date,
+	_Status			int
     -- PHU_ID bigint FOREIGN KEY REFERENCES PHU(PHU_ID),
     -- Age_Group_ID bigint FOREIGN KEY REFERENCES Age_Group(Age_Group_ID),
     -- Gender_ID bigint FOREIGN KEY REFERENCES Gender(Gender_ID),
 )
 
 CREATE TABLE DataSource(
-	SourceID bigint IDENTITY(1,1) PRIMARY KEY,
-	SourceName nvarchar(300)
+	SourceID		bigint IDENTITY(1,1) PRIMARY KEY,
+	SourceName		nvarchar(300)
 )
 /*
 1 - Case_Report
 2 - Compiled_COVID_19_Case_Details_Canada
 3 - vaccines_by_age_phu
-
 */
 
-truncate table COMPILED_COVID_19_CASE_DETAILS_CANADA_NDS
+drop table COMPILED_COVID_19_CASE_DETAILS_CANADA_NDS
 -- SELECT --------------------------------------------------------------------------------
 SELECT * FROM PHU_CITY_NDS
 SELECT * FROM PHU_GROUP_NDS
@@ -224,7 +240,8 @@ SELECT * FROM AGE_GROUP_NDS
 SELECT * FROM GENDER_NDS
 SELECT * FROM COMPILED_COVID_19_CASE_DETAILS_CANADA_NDS
 
-
+select
+from phu_nds phu join PHU_CITY_NDS phuc on (phu.phu_city_id = phuc.
 -- ADD FOREIGN KEY --------------------------------------------------------------------------------
 --Chua chinh khoa ngoai
 -- PHU_City_NDS(PHU_Group_ID) REFERENCES PHU_Group_NDS(PHU_Group_ID)
