f8630d3ee7cc21a3862547d0783aaa59d7dbf28e
Thuong Nguyen <thaonhucute2001@gmail.com>
2023-01-06 13:13:53 +0700

    minig script

--- Script/Mining.sql
+++ Script/Mining.sql
@@ -1,6 +1,8 @@
 USE DATH_DDS
 GO
 
+DROP VIEW IF EXISTS dbo.vCaseDetail
+GO
 CREATE VIEW vCaseDetail
 AS
 	SELECT 
@@ -16,6 +18,7 @@ AS
 		, o.Outcome_Name
 		, a.Age_Name
 		, cs.Case_Status_Name
+		, c.ToTalCase
 	FROM Fact_Case c
 		JOIN dim_Phu p ON p.PHU_ID = c.PHU_ID
 		JOIN dim_Date d ON d.IDDate = c.IDDate
@@ -25,6 +28,8 @@ AS
 		JOIN dim_Case_Status cs ON cs.Case_Status_ID = c.Case_Status_ID
 GO
 
+DROP VIEW IF EXISTS dbo.vVacinated
+GO
 CREATE VIEW vVacinated
 AS
 	SELECT 
@@ -46,6 +51,9 @@ AS
 		JOIN dim_Date d ON d.IDDate = c.IDDate
 		JOIN dim_Age a ON a.Age_ID = c.Age_ID
 GO
+
+DROP TABLE Mining_CaseDetail 
+DROP TABLE Mining_Vacinated
 
 SELECT * INTO Mining_CaseDetail FROM vCaseDetail
 GO
