34f282b1f7434e044c94118d1553409183230fd7
Thuong Nguyen <thaonhucute2001@gmail.com>
2023-01-06 13:01:28 +0700

    edit mining script + mining

---
+++ Script/Mining.sql
@@ -0,0 +1,53 @@
+USE DATH_DDS
+GO
+
+CREATE VIEW vCaseDetail
+AS
+	SELECT 
+		ROW_NUMBER() OVER (ORDER BY c.PHU_ID, c.IDDate, c.Age_ID, c.Gender_ID, c.Outcome_ID, c.Case_Status_ID) AS ID
+		, p.Reporting_PHU
+		, p.PHU_City
+		, p.PHU_GROUP
+		, d.FullDateTime
+		, d.Thang
+		, d.Quy
+		, d.Nam
+		, g.Gender_Name
+		, o.Outcome_Name
+		, a.Age_Name
+		, cs.Case_Status_Name
+	FROM Fact_Case c
+		JOIN dim_Phu p ON p.PHU_ID = c.PHU_ID
+		JOIN dim_Date d ON d.IDDate = c.IDDate
+		JOIN dim_Gender g ON g.Gender_ID = c.Gender_ID
+		JOIN dim_Outcome o ON o.Outcome_ID = c.Outcome_ID
+		JOIN dim_Age a ON a.Age_ID = c.Age_ID
+		JOIN dim_Case_Status cs ON cs.Case_Status_ID = c.Case_Status_ID
+GO
+
+CREATE VIEW vVacinated
+AS
+	SELECT 
+		ROW_NUMBER() OVER (ORDER BY c.PHU_ID, c.IDDate, c.Age_ID) AS ID
+		, p.Reporting_PHU
+		, p.PHU_City
+		, p.PHU_GROUP
+		, d.FullDateTime
+		, d.Thang
+		, d.Quy
+		, d.Nam
+		, a.Age_Name
+		, c.OneDoseCumulative
+		, c.SecondDoseCumulative
+		, c.FullyVaccinated
+		, c.ThirdDoseCumulative
+	FROM Fact_Vacinated c
+		JOIN dim_Phu p ON p.PHU_ID = c.PHU_ID
+		JOIN dim_Date d ON d.IDDate = c.IDDate
+		JOIN dim_Age a ON a.Age_ID = c.Age_ID
+GO
+
+SELECT * INTO Mining_CaseDetail FROM vCaseDetail
+GO
+SELECT * INTO Mining_Vacinated FROM vVacinated
+GO
